6ca7808797617cffb5b3b739dbec48a54725912a
Talgat <duzbatyrovtalgat96@gmail.com>
2020-02-14 21:18:27 +0400

    Рефакторинг

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,91 +5,28 @@ import RandomPlanet from '../random-planet';
 import ErrorBoundry from '../error-boundry';
 
 import Row from "../row/row";
-import ItemDetails, { Record } from "../item-details/item-details";
 import SwapiService from "../../services/swapi-service";
 
-import { SwapiServiceProvider } from '../swapi-service-context'
-
-import {
-  PersonDetails,
-  PlanetDetails,
-  StarshipDetails,
-  PersonList,
-  PlanetList,
-  StarshipList
-} from '../sw-components'
+import { SwapiServiceProvider } from '../swapi-service-context';
+import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
 
 import './app.css';
 
 export default class App extends Component {
 
   swapiService = new SwapiService();
-
-  state = {
-    showRandomPlanet: true
-  };
-
-  toggleRandomPlanet = () => {
-    this.setState((state) => {
-      return {
-        showRandomPlanet: !state.showRandomPlanet
-      }
-    });
-  };
-
   render() {
-
-    const planet = this.state.showRandomPlanet ?
-      <RandomPlanet/> :
-      null;
-
-    const { getPerson,
-            getStarship,
-            getPersonImage,
-            getStarshipImage } = this.swapiService;
-
-    const personDetails = (
-      <ItemDetails
-        itemId={11}
-        getData={getPerson}
-        getImageUrl={getPersonImage} >
-
-        <Record field="gender" label="Gender:" />
-        <Record field="eyeColor" label="Eye Color:" />
-
-      </ItemDetails>
-    );
-
-    const starshipDetails = (
-      <ItemDetails
-        itemId={5}
-        getData={getStarship}
-        getImageUrl={getStarshipImage}>
-
-        <Record field="model" label="Model:" />
-        <Record field="length" label="Length:" />
-        <Record field="passengers" label="Passengers:" />
-      </ItemDetails>
-
-    );
-
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.swapiService}>
           <div className="stardb-app">
             <Header />
 
-            <PersonDetails itemId={7}/>
+            <RandomPlanet/>
+            <PeoplePage />
+            <PlanetsPage />
+            <StarshipsPage />
 
-            <PlanetDetails itemId={1}/>
-
-            <StarshipDetails itemId={9}/>
-
-            <PersonList />
-
-            <StarshipList />
-
-            <PlanetList />
           </div>
         </SwapiServiceProvider>
       </ErrorBoundry>
